1fff49800e77b00c931c7db008936c54e928a107
renequez <ecristianbujor@gmail.com>
2021-02-09 11:56:53 +0200

    finished project 15

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
-  return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+	return (
+		<section className='about-section section'>
+			<h1 className='section-title'>About us</h1>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique doloribus placeat voluptatem illum, eligendi fugiat optio quasi atque
+				laboriosam ipsa, consequatur, temporibus harum odit necessitatibus tempore deserunt perferendis non repudiandae fugit enim nemo cumque odio
+				neque error! Excepturi incidunt labore dicta exercitationem cumque maxime, quod dolor fugit quia reprehenderit nesciunt nam consequatur
+				aspernatur laborum. Numquam, earum harum. A laboriosam, nesciunt ratione in ullam hic atque voluptatem aliquam assumenda quaerat impedit nulla
+				ipsa sunt, accusantium, illo commodi numquam! Doloribus dignissimos amet quaerat aut ipsum, impedit molestias nobis nesciunt quia, sequi soluta
+				ea aspernatur in pariatur ab eaque. Impedit minus reiciendis alias.
+			</p>
+		</section>
+	);
+};
 
-export default About
+export default About;
